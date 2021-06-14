504ca6d5e53bf55304bf6484545344caa81faeaa
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2021-06-14 14:26:05 -0800

    Update CreateSQLTables.ps1
    Updated referenced file name

--- CreateSQLTables.ps1
+++ CreateSQLTables.ps1
@@ -20,4 +20,4 @@ $sqlParams = [ordered]@{
 $connString = 'Server={0};Database={1};User Id={2};Password={3};' -f [array]$sqlParams.Values
 
 # Run SQL query to Create SQL Tables
-Invoke-Sqlcmd -ConnectionString $connString -InputFile 'CreateTables.sql'
+Invoke-Sqlcmd -ConnectionString $connString -InputFile 'CreateSQLTables.sql'
