e02026de65cefeb78fb1433c9b4d34f88b34c2f8
robgilbreath <46797100+robgilbreath@users.noreply.github.com>
2021-06-16 16:16:57 -0800

    Distinguish static and time-varying data in devices
    Datto's 'devices' endpoint contains a mix of static and time-varying data. These changes separate the 'devices' table into two so that historical movement can be captured without excessive redundancy.

--- CreateSQLTables.sql
+++ CreateSQLTables.sql
@@ -28,23 +28,30 @@ CREATE TABLE [drmm].[devices](
 	[description] VARCHAR(MAX),
 	[deviceType] VARCHAR(MAX),
 	[deviceClass] VARCHAR(MAX),
-	[dotNetVersion] VARCHAR(MAX),
 	[manufacturer] VARCHAR(MAX),
 	[model] VARCHAR(MAX),
 	[serialnumber] VARCHAR(MAX),
 	[cpuCores] INT,
 	[memory] INT,
 	[domain] VARCHAR(MAX),
-	[username] VARCHAR(MAX),
-	[lastSeenUser] VARCHAR(MAX),
-	[ipaddress] VARCHAR(MAX),
-	[os] VARCHAR(MAX),
 	[is64bit] BIT,
 	[snmpEnabled] BIT,
 	[suspended] BIT,
 	[deleted] BIT,
-	[rebootRequired] BIT,
 	[warrantyDate] DATETIME,
+	[lastUpdate] DATETIME
+)
+GO
+
+IF OBJECT_ID ('drmm.devices_timevary') IS NULL
+CREATE TABLE [drmm].[devices_timevary](
+	[id] INT NOT NULL PRIMARY KEY,
+	[dotNetVersion] VARCHAR(MAX),
+	[username] VARCHAR(MAX),
+	[lastSeenUser] VARCHAR(MAX),
+	[ipaddress] VARCHAR(MAX),
+	[os] VARCHAR(MAX),
+	[rebootRequired] BIT,
 	[lastAudit] DATETIME,
 	[lastReboot] DATETIME,
 	[lastSeen] DATETIME,
@@ -55,7 +62,6 @@ GO
 IF OBJECT_ID('drmm.FK_devices_site') IS NULL
 ALTER TABLE [drmm].[devices] WITH CHECK ADD CONSTRAINT [FK_devices_site] FOREIGN KEY([siteId]) REFERENCES [drmm].[sites] ([id])
 GO
-
 
 IF OBJECT_ID ('drmm.patchstatus') IS NULL
 CREATE TABLE [drmm].[patchstatus](
@@ -216,23 +222,31 @@ CREATE TABLE [temp].[devices](
 	[description] VARCHAR(MAX),
 	[deviceType] VARCHAR(MAX),
 	[deviceClass] VARCHAR(MAX),
-	[dotNetVersion] VARCHAR(MAX),
 	[manufacturer] VARCHAR(MAX),
 	[model] VARCHAR(MAX),
 	[serialnumber] VARCHAR(MAX),
 	[cpuCores] INT,
 	[memory] INT,
 	[domain] VARCHAR(MAX),
-	[username] VARCHAR(MAX),
-	[lastSeenUser] VARCHAR(MAX),
-	[ipaddress] VARCHAR(MAX),
-	[os] VARCHAR(MAX),
 	[is64bit] BIT,
 	[snmpEnabled] BIT,
 	[suspended] BIT,
 	[deleted] BIT,
-	[rebootRequired] BIT,
 	[warrantyDate] DATETIME,
+	[lastUpdate] DATETIME
+)
+
+GO
+
+IF OBJECT_ID ('temp.devices_timevary') IS NULL
+CREATE TABLE [temp].[devices_timevary](
+	[id] INT NOT NULL PRIMARY KEY,
+	[dotNetVersion] VARCHAR(MAX),
+	[username] VARCHAR(MAX),
+	[lastSeenUser] VARCHAR(MAX),
+	[ipaddress] VARCHAR(MAX),
+	[os] VARCHAR(MAX),
+	[rebootRequired] BIT,
 	[lastAudit] DATETIME,
 	[lastReboot] DATETIME,
 	[lastSeen] DATETIME,
